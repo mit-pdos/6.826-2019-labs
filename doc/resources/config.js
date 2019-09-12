bc71ffaa2d7e40c5456721bd7b316281159571a5
Tej Chajed <tchajed@mit.edu>
2019-09-12 16:54:48 -0400

    update

--- doc/resources/config.js
+++ doc/resources/config.js
@@ -2,6 +2,17 @@ var coqdocjs = coqdocjs || {};
 
 coqdocjs.repl = {};
 
-coqdocjs.subscr = {};
+coqdocjs.subscr = {
+  "0": "0",
+  "1": "1",
+  "2": "2",
+  "3": "3",
+  "4": "4",
+  "5": "5",
+  "6": "6",
+  "7": "7",
+  "8": "8",
+  "9": "9"
+};
 
 coqdocjs.replInText = [];
